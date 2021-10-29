d5c197f40dae7b3092defd2254383161f2dc1fe7
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-10-29 17:02:33 +0200

    Play sound

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ class DrumKit {
         //loop over pads
         activeBars.forEach(bar => {
             bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            //check if pads are active
+            if(bar.classList.contains('active')) {
+                //check each sound
+                if(bar.classList.contains('kick-pad')){
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')){
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+
+            }
         });
         this.index++;
     }
